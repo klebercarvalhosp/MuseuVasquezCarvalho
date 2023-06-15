f75989965b5385d8513ae05992a3c99b009a50c8
LucasOcampos <lucas.bin.ocampos@gmail.com>
2023-06-15 18:06:16 -0300

    Autenticação adicionada e funcional

--- scripts/login.js
+++ scripts/login.js
@@ -1,78 +1,95 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const usernames = document.getElementsByClassName("username");
+const passwords = document.getElementsByClassName("password");
+const emailRecoveryButton = document.getElementById('email-recovery');
+
+for (let username of usernames) {
+    username.addEventListener('change', () => {
+        username.style.border = "0.1em solid transparent";
+        username.style.backgroundImage = "linear-gradient(#eee, #eee), " +
+            "linear-gradient(120deg, #fcb045 10%, #fd1d1d 35%, #833ab4 100%)";
+    });
+}
+
+for (let password of passwords) {
+    password.addEventListener('change', () => {
+        password.style.border = "0.1em solid transparent";
+        password.style.backgroundImage = "linear-gradient(#eee, #eee), " +
+            "linear-gradient(120deg, #fcb045 10%, #fd1d1d 35%, #833ab4 100%)";
+    });
+}
+
+emailRecoveryButton.addEventListener('click', resetEmail);
 
 signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
-    var elements = document.getElementsByClassName("username");
-    for (element of elements) {
-        element.value = "";
-    }
+    resetCredentialFields();
 });
 
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
-    var elements = document.getElementsByClassName("username");
-    for (element of elements) {
+    resetCredentialFields();
+});
+
+function resetCredentialFields() {
+    for (let username of usernames) {
+        username.style.border = "0.1em solid transparent";
+        username.style.backgroundImage = "linear-gradient(#eee, #eee), " +
+            "linear-gradient(120deg, #fcb045 10%, #fd1d1d 35%, #833ab4 100%)";
+    }
+
+    for (let password of passwords) {
+        password.style.border = "0.1em solid transparent";
+        password.style.backgroundImage = "linear-gradient(#eee, #eee), " +
+            "linear-gradient(120deg, #fcb045 10%, #fd1d1d 35%, #833ab4 100%)";
+    }
+
+    for (let element of usernames) {
         element.value = "";
     }
-});
+    for (let element of passwords) {
+        element.value = "";
+    }
+}
 
 function cadastrarUsuario() {
     event.preventDefault();
-    var email = "";
-    var password = "";
-    var elements = document.getElementsByClassName("username");
-    for (element of elements) {
+    let email = "";
+    let password = "";
+    for (let element of usernames) {
         if (element.value) {
             email = element.value;
         }
     }
-    var elements = document.getElementsByClassName("password");
-    for (element of elements) {
+    for (let element of passwords) {
         if (element.value) {
             password = element.value;
         }
     }
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            var user = userCredential.user;
-            console.log("Usuário criado com sucesso!");
+        .then(() => {
+            loginUser(email, password);
         })
         .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            if (errorCode == "auth/invalid-email") {
-                var email = "";
-                var elements = document.getElementsByClassName("username");
-                for (element of elements) {
-                    if (element.value) {
-                        email = element;
-                    }
-                }
-                // add red border
-            }
-            else {
-                console.log(`Status ${errorCode}: ${errorMessage}`);
-            }
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            treatCredentialErrors("sign-up", errorCode, errorMessage);
         });
-
-    loginUser(email, password);
 }
 
 function logarUsuario() {
     event.preventDefault();
-    var email = "";
-    var password = "";
-    var elements = document.getElementsByClassName("username");
-    for (element of elements) {
+    let email = "";
+    let password = "";
+    for (let element of usernames) {
         if (element.value) {
             email = element.value;
         }
     }
-    var elements = document.getElementsByClassName("password");
-    for (element of elements) {
+    for (let element of passwords) {
         if (element.value) {
             password = element.value;
         }
@@ -84,31 +101,70 @@ function logarUsuario() {
 function loginUser(email, password) {
     event.preventDefault();
     firebase.auth().signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            var user = userCredential.user;
-            console.log("Usuário logado com sucesso!");
+        .then(() => {
             successfulLogin();
         })
         .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            if (errorCode == "auth/invalid-email") {
-                var email = "";
-                var elements = document.getElementsByClassName("username");
-                for (element of elements) {
-                    if (element.value) {
-                        email = element;
-                    }
-                }
-                // add red border
-            }
-            else if (errorCode == "auth/user-not-found") {
-                // move to sign up
-            }
-            else {
-                console.log(`Status ${errorCode}: ${errorMessage}`);
-            }
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            treatCredentialErrors("login", errorCode, errorMessage);
         });
+}
+
+function treatCredentialErrors(method, errorCode, errorMessage) {
+    let selector = method === "login" ? ".sign-in-container" : ".sign-up-container";
+
+    if (errorCode === "auth/invalid-email") {
+        let email = (
+            document
+                .querySelector(selector)
+                .getElementsByClassName("username")
+        )[0];
+
+        email.style.border = "0.15em solid transparent";
+        email.style.backgroundImage = "linear-gradient(#eee, #eee), linear-gradient(#Ff0042, #Ff0042)";
+        email.focus();
+        email.select();
+    } else if (errorCode === "auth/user-not-found") {
+        let current_email = (
+            document
+                .querySelector(".sign-in-container")
+                .getElementsByClassName("username")
+        )[0].value;
+        let current_password = (
+            document
+                .querySelector(".sign-in-container")
+                .getElementsByClassName("password")
+        )[0].value;
+
+        document.getElementById("signUp").click();
+
+        (
+            document
+                .querySelector(".sign-up-container")
+                .getElementsByClassName("username")[0]
+                .value
+        ) = current_email;
+        (
+            document
+                .querySelector(".sign-up-container")
+                .getElementsByClassName("password")[0]
+                .value
+        ) = current_password;
+    } else if (errorCode === "auth/wrong-password") {
+        let password = (
+            document
+                .querySelector(selector)
+                .getElementsByClassName("password")
+        )[0];
+
+        password.style.border = "0.15em solid transparent";
+        password.style.backgroundImage = "linear-gradient(#eee, #eee), linear-gradient(#Ff0042, #Ff0042)";
+        password.focus();
+        password.select();
+    } else {
+        console.log(`Status ${errorCode}: ${errorMessage}`);
+    }
 }
 
 function successfulLogin() {
@@ -129,4 +185,20 @@ function toggleHidden() {
         main_site.setAttribute("hidden", "hidden");
         login.removeAttribute("hidden");
     }
+}
+
+function resetEmail() {
+    let email = (
+        document
+            .querySelector(".sign-in-container")
+            .getElementsByClassName("username")
+    )[0].value;
+
+    firebase.auth().sendPasswordResetEmail(email)
+        .then(() => {})
+        .catch((error) => {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            treatCredentialErrors("login", errorCode, errorMessage);
+        });
 }
