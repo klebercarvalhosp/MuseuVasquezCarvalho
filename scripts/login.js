40e4e479660e7ed8218c5f3bd6e76d41b1c6c654
Lucas Ocampos <lucas.bin.ocampos@gmail.com>
2023-06-16 21:21:48 -0300

    Adding card creation

--- scripts/login.js
+++ scripts/login.js
@@ -101,9 +101,7 @@ function logarUsuario() {
 function loginUser(email, password) {
     event.preventDefault();
     firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(() => {
-            successfulLogin();
-        })
+        .then(successfulLogin())
         .catch((error) => {
             let errorCode = error.code;
             let errorMessage = error.message;
@@ -148,6 +146,32 @@ function treatCredentialErrors(method, errorCode, errorMessage) {
         (
             document
                 .querySelector(".sign-up-container")
+                .getElementsByClassName("password")[0]
+                .value
+        ) = current_password;
+    } else if (errorCode === "auth/email-already-in-use") {
+        let current_email = (
+            document
+                .querySelector(".sign-up-container")
+                .getElementsByClassName("username")
+        )[0].value;
+        let current_password = (
+            document
+                .querySelector(".sign-up-container")
+                .getElementsByClassName("password")
+        )[0].value;
+
+        document.getElementById("signIn").click();
+
+        (
+            document
+                .querySelector(".sign-in-container")
+                .getElementsByClassName("username")[0]
+                .value
+        ) = current_email;
+        (
+            document
+                .querySelector(".sign-in-container")
                 .getElementsByClassName("password")[0]
                 .value
         ) = current_password;
